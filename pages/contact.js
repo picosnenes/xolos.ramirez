d2e840c01927f17660376d761b20eca5574127f5
picosnenes <alexaregut@gmail.com>
2024-08-13 07:04:18 -0600

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+// pages/contact.js
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Contact() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Xolos Ramirez - Contacto</title>
+               
